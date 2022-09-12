fb6f6e657a590d97e43bbd3fb1334dae3aef6259
blitzmartin <lizmartinmusic@gmail.com>
2022-09-12 18:24:38 +0200

    successfully import and map  from works data

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -1,71 +1,27 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import data from "../data"
+import {data} from "../data"
 
 export default function PortfolioItem() {
-    return(
-        <>
-
-        {
-           data.map((work) =>{
-            <div className="portfolio-item">
-            <div>
-              <iframe src="https://www.youtube.com/embed/1H9zmucUAas"/>
-            </div>
-            <div>
-                <Card.Body style={{ textAlign: "right" }}>
-                  <Card.Title>{work.title}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                  {work.subtitle}
-                  </Card.Subtitle>
-                  <Card.Text>Directed by: {work.director}</Card.Text>
-                  <Card.Text>
-                    Analytics: {work.analytics}
-                  </Card.Text>
-                </Card.Body>
-            </div>
-          </div>
-           })
-         }
-
-
-         
-         <div className="portfolio-item">
+  return (
+    <>
+      {data.map((work) => (
+        <div className="portfolio-item" key={work.videoID}>
           <div>
-            <iframe src="https://www.youtube.com/embed/1H9zmucUAas" />
+            <iframe src={`https://www.youtube.com/embed/${work.videoID}`} />
           </div>
           <div>
-              <Card.Body style={{ textAlign: "right" }}>
-                <Card.Title>X-Factor / Sky (2018)</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  Music, audio post-production, mix and master
-                </Card.Subtitle>
-                <Card.Text>Directed by: Francesco Ebbasta</Card.Text>
-                <Card.Text>
-                  Analytics: 20M views, reach 70M users Winner Grand Prix BC&E
-                  Award 2019
-                </Card.Text>
-              </Card.Body>
+            <Card.Body style={{ textAlign: "right" }}>
+              <Card.Title>{work.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                {work.subtitle}
+              </Card.Subtitle>
+              <Card.Text>Directed by: {work.director}</Card.Text>
+              <Card.Text>Analytics: {work.analytics}</Card.Text>
+            </Card.Body>
           </div>
         </div>
-
-        <div className="portfolio-item">
-          <div>
-            <iframe src="https://www.youtube.com/embed/eSmNnwUmtcc" />
-          </div>
-          <div>
-              <Card.Body style={{ textAlign: "right" }}>
-                <Card.Title>Netflix Italia - Narcos 3 (2017)</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">
-                  Music, audio post-production, mix and master
-                </Card.Subtitle>
-                <Card.Text>Directed by: Mattia Croccia</Card.Text>
-                <Card.Text>
-                Analytics: 1.7M
-                </Card.Text>
-              </Card.Body>
-          </div>
-        </div>
-        </>
-    )
+      ))}
+    </>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 export const data = [
-    {
+{
     videoID: "1H9zmucUAas",
     title: "X-Factor / Sky (2018)",
     subtitle: "Music, audio post-production, mix and master",
     director: "Francesco Ebbasta",
     analytics: "20M views, reach 70M users Winner Grand Prix 2019"
+},
+{
+    videoID: "eSmNnwUmtcc",
+    title: "Netflix Italia - Narcos 3 (2017)",
+    subtitle: "Music, audio post-production, mix and master",
+    director: "Mattia Croccia",
+    analytics: "1.7M"
 },
 ]
