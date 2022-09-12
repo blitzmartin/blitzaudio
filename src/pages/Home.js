91198e0216d6ea9ad98036080f48d0f6136cd648
blitzmartin <lizmartinmusic@gmail.com>
2022-09-12 16:00:28 +0200

    centered home page text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-image"></div>
-      <div className="overlay">
+      <div className="overlay home-container">
         <h1>BLITZ AUDIO</h1>
         <h5>Sound solutions for everyone</h5>
       </div>
