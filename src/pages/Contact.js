5e0b4ab4aaa609e03c63a896b3ff77007bab8c3c
blitzmartin <lizmartinmusic@gmail.com>
2022-09-13 13:44:51 +0200

    redirecting on submit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,73 +1,75 @@
-import { send } from 'emailjs-com';
-import React, { useState } from 'react'
+import { send } from "emailjs-com";
+import React, { useState } from "react";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 
 export default function Contact() {
 
+  const navigate = useNavigate();
   const [toSend, setToSend] = useState({
-    from_name: '',
-    message: '',
-    reply_to: '',
+    from_name: "",
+    message: "",
+    reply_to: "",
   });
 
   const onSubmit = (e) => {
     e.preventDefault();
-    send(
-      'service_aw96jtb',
-      'template_d6u6m5y',
-      toSend,
-      'Da8JZcAKED34WQhbQ'
-    )
+    send("service_aw96jtb", "template_d6u6m5y", toSend, "Da8JZcAKED34WQhbQ")
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        console.log("SUCCESS!", response.status, response.text);
         setToSend({
-          from_name: '',
-          message: '',
-          reply_to: '',
-        })
+          from_name: "",
+          message: "",
+          reply_to: "",
+        });
+        navigate('/');
       })
       .catch((err) => {
-        console.log('FAILED...', err);
+        console.log("FAILED...", err);
       });
   };
 
   const handleChange = (e) => {
     setToSend({
-      ...toSend, [e.target.name]: e.target.value
+      ...toSend,
+      [e.target.name]: e.target.value,
     });
   };
 
   return (
     <div className="contact">
       <div className="section">
-        <h1 className='newPosth1'>Contact me</h1>
+        <h1 className="newPosth1">Contact me</h1>
         <form onSubmit={onSubmit}>
           <input
-            type='text'
-            name='from_name'
-            placeholder='Your name'
+            type="text"
+            name="from_name"
+            placeholder="Your name"
             value={toSend.from_name}
             onChange={handleChange}
             required
           />
           <input
-            type='text'
-            name='reply_to'
-            placeholder='Your email'
+            type="text"
+            name="reply_to"
+            placeholder="Your email"
             value={toSend.reply_to}
             onChange={handleChange}
             required
           />
           <textarea
-            rows="18" col="20"
-            name='message'
-            placeholder='Your message'
+            rows="18"
+            col="20"
+            name="message"
+            placeholder="Your message"
             value={toSend.message}
             onChange={handleChange}
             required
           />
-          <button className="submitBtn" type='submit'>Submit</ button>
+          <button className="submitBtn" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
