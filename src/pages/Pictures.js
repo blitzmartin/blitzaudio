cd4e04e55a0740e4ea6eae43bdf9448437805c75
blitzmartin <lizmartinmusic@gmail.com>
2022-09-13 11:58:05 +0200

    picture page, no grid

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { data } from "../pics";
 
 export default function Pictures(){
     return(
-        <div className="center-align">
-            <h1>Pictures</h1>
-        </div>
+         <>
+         <div className="center-align">
+           <div className="bg-image"></div>
+           <div className="overlay portfolio-container">
+             <div className="pics-container">
+               {data.map((pic) => (
+                 <div key={pic.name}>
+                   <img className='grid-img'
+                     src={`./images/liz/${pic.name}`}
+                   />
+                 </div>
+               ))}
+             </div>
+           </div>
+         </div>
+       </>
     )
 }
---
+++ src/pics.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        name: "01.jpg"
+    },
+    {
+        name: "02.jpg"
+    },
+    {
+        name: "03.jpg"
+    },
+    {
+        name: "04.jpg"
+    },
+    {
+        name: "05.jpg"
+    },
+    {
+        name: "06.jpg"
+    },
+    {
+        name: "07.jpg"
+    },
+    {
+        name: "08.jpg"
+    },
+    {
+        name: "09.jpg"
+    },
+    {
+        name: "10.jpg"
+    },
+    {
+        name: "11.jpg"
+    },
+    {
+        name: "12.jpg"
+    },
+    {
+        name: "13.jpg"
+    },
+    {
+        name: "14.jpg"
+    },
+]
