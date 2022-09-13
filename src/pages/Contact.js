00fe9ebdfaebb50d6072aba5eee1713ffafea5b6
blitzmartin <lizmartinmusic@gmail.com>
2022-09-13 12:05:49 +0200

    added contact page and itchio link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import { send } from 'emailjs-com';
+import React, { useState } from 'react'
 
-export default function Contact(){
-    return(
-        <div>
-            <h1>Contact Us</h1>
-        </div>
+export default function Contact() {
+
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_aw96jtb',
+      'template_d6u6m5y',
+      toSend,
+      'Da8JZcAKED34WQhbQ'
     )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setToSend({
+          from_name: '',
+          message: '',
+          reply_to: '',
+        })
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({
+      ...toSend, [e.target.name]: e.target.value
+    });
+  };
+
+  return (
+    <div className="contact">
+      <div className="section">
+        <h1 className='newPosth1'>Contact me</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            name='from_name'
+            placeholder='Your name'
+            value={toSend.from_name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type='text'
+            name='reply_to'
+            placeholder='Your email'
+            value={toSend.reply_to}
+            onChange={handleChange}
+            required
+          />
+          <textarea
+            rows="18" col="20"
+            name='message'
+            placeholder='Your message'
+            value={toSend.message}
+            onChange={handleChange}
+            required
+          />
+          <button className="submitBtn" type='submit'>Submit</ button>
+        </form>
+      </div>
+    </div>
+  )
 }
--- src/pages/Works.js
+++ src/pages/Works.js
@@ -8,7 +8,7 @@ export default function Works() {
       <div className="bg-image"></div>
       <div className="overlay portfolio-container">
         <h2>Works</h2>
-        <p>Blitzaudio took part in the production of hundreds of commercials and short movies, creating original music and curating the many stages of audio post-production including dubbing, sound design, dialogue editing, music selection, music editing, mixing and mastering.  Check out a selection of our works and contact us to watch something that fits your job best.</p>
+        <p>Blitzaudio took part in the production of hundreds of commercials and short movies, creating original music and curating the many stages of audio post-production including dubbing, sound design, dialogue editing, music selection, music editing, mixing and mastering.  Check out the following selection of works and contact us to watch something that fits your job best. For gaming sound design and music check <a href="https://blitzaudio.itch.io/" target="_blank">Itch.io page</a>.</p>
        <PortfolioItem />
       </div>
     </div>
